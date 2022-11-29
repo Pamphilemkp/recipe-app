2976ff2d6f168fc8a3a623bbfc45e72774c23da0
pamphilemkp <pamphilemkp@gmail.com>
2022-11-29 14:04:30 +0200

    add registration and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
+
+  resources :users, only: %i[index show] do
+    resources :foods, only: %i[index show new create destroy] do
+      resources :recipes, only: %i[index show]
+    end
+
+    resources :recipes, only: %i[index show new create destroy] do
+      resources :recipes, only: %i[index show]
+    end
+  end
 end
